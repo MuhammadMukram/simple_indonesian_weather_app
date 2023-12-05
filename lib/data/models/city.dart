4936a6d03a75a70b7bdf30bc7568ef88ccdca151
mukramm_ <99308248+MuhammadMukram@users.noreply.github.com>
2023-12-05 23:15:51 +0800

    refactor: remove unused comment

--- lib/data/models/city.dart
+++ lib/data/models/city.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-// {"Cerah", "Cerah Berawan", "Berawan", "Kabut", "Berasap", "Hujan Ringan", "Hujan Sedang", "Hujan Lebat / Petir"}
 class City {
   final String? cityName;
   final List<dynamic>? temperature;
