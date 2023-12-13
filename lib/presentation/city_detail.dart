b51c821819cc905e3c0d81385c14cebc238b8bbd
mukramm_ <99308248+MuhammadMukram@users.noreply.github.com>
2023-12-13 15:51:15 +0800

    feat: added the bmkg logo as a data retrieval source

--- lib/presentation/city_detail.dart
+++ lib/presentation/city_detail.dart
@@ -23,7 +23,51 @@ class CityDetail extends StatelessWidget {
           ),
           centerTitle: true,
           actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.info_outline)),
+            IconButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Dialog(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0)),
+                          child: Container(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 200,
+                                  padding: const EdgeInsets.all(20),
+                                  child: Image.asset(
+                                    'assets/images/Logo-BMKG-new.png',
+                                  ),
+                                ),
+                                RichText(
+                                  text: const TextSpan(
+                                    style: TextStyle(color: Colors.black),
+                                    children: <TextSpan> [
+                                      TextSpan(
+                                        text: "The data from this application is obtained from BMKG Indonesia's weather API via the website: \n\n",
+                                        style: TextStyle(fontSize: 18)
+                                      ),
+                                      TextSpan(
+                                        text: "https://data.bmkg.go.id/prakiraan-cuaca/",
+                                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)
+                                      )
+                                    ]
+                                  ),
+                                )
+                                // const Text(
+                                //     "The data from this application is obtained from BMKG Indonesia's weather API via the website https://data.bmkg.go.id/prakiraan-cuaca/")
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                },
+                icon: const Icon(Icons.info_outline)),
           ],
           backgroundColor: ColorConstants.kDarkPurpleColor,
         ),
@@ -31,118 +75,113 @@ class CityDetail extends StatelessWidget {
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.only(top: 30.0, left: 10.0, right: 10.0),
-            child: Expanded(
-              flex: 1,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    city.cityName!,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      color: ColorConstants.kDarkPurpleColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                    ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  city.cityName!,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: ColorConstants.kDarkPurpleColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
                   ),
-                  const SizedBox(height: 40),
-                  Expanded(
-                    child: GridView.count(
-                      mainAxisSpacing: 5,
-                      crossAxisSpacing: 5,
-                      childAspectRatio: 3 / 5,
-                      crossAxisCount: 2,
-                      children: List.generate(4, (index) {
-                        String weather = city.weather![index]["value"]["\$t"];
-                        List temperature = city.temperature![index]["value"];
-                        return ClipRRect(
-                          borderRadius: BorderRadius.circular(12.0),
-                          child: Container(
-                            decoration: const BoxDecoration(
-                                color: ColorConstants.kWhite,
-                                boxShadow: [
-                                  BoxShadow(
-                                      color: Color.fromARGB(10, 0, 0, 0),
-                                      blurRadius: 5.0,
-                                      offset: Offset(2, 2))
-                                ]),
-                            padding: const EdgeInsets.all(12),
-                            child: Column(
+                ),
+                const SizedBox(height: 35),
+                GridView.count(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 5,
+                  childAspectRatio: 3 / 5,
+                  crossAxisCount: 2,
+                  children: List.generate(4, (index) {
+                    String weather = city.weather![index]["value"]["\$t"];
+                    List temperature = city.temperature![index]["value"];
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(12.0),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            color: ColorConstants.kWhite,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color.fromARGB(10, 0, 0, 0),
+                                  blurRadius: 5.0,
+                                  offset: Offset(2, 2))
+                            ]),
+                        padding: const EdgeInsets.all(15),
+                        child: Column(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(5),
+                              child:
+                                  Lottie.asset('${weatherCode[weather]![1]}'),
+                            ),
+                            Text(
+                              "${weatherCode[weather]![0]}",
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Column(
                               children: [
-                                Expanded(
-                                  flex: 1,
-                                  child: Lottie.asset(
-                                      '${weatherCode[weather]![1]}'),
+                                const Text(
+                                  "Time",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
                                 Text(
-                                  "${weatherCode[weather]![0]}",
-                                  textAlign: TextAlign.center,
+                                  timeValue[index],
+                                  style: const TextStyle(
+                                    fontSize: 12,
+                                  ),
                                 ),
-                                const SizedBox(
-                                  height: 10,
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                const Text(
+                                  "Temperature",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12),
                                 ),
-                                Column(
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
-                                    const Text(
-                                      "Time",
-                                      style: TextStyle(
+                                    Text(
+                                      "${temperature[0]["\$t"]} °C",
+                                      style: const TextStyle(
                                         fontSize: 12,
-                                        fontWeight: FontWeight.bold,
                                       ),
                                     ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
                                     Text(
-                                      timeValue[index],
+                                      "${temperature[1]["\$t"]} °F",
                                       style: const TextStyle(
                                         fontSize: 12,
                                       ),
                                     ),
                                   ],
                                 ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    const Text(
-                                      "Temperature",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 12),
-                                    ),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        Text(
-                                          "${temperature[0]["\$t"]} °C",
-                                          style: const TextStyle(
-                                            fontSize: 12,
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          width: 10,
-                                        ),
-                                        Text(
-                                          "${temperature[1]["\$t"]} °F",
-                                          style: const TextStyle(
-                                            fontSize: 12,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
                               ],
                             ),
-                          ),
-                        );
-                      }),
-                    ),
-                  ),
-                ],
-              ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ],
             ),
           ),
         ));
